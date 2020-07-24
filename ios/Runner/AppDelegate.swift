d3e238fccfc6b100622cf9d0b194d539f2df0448
divjot-bst <divjot.singh@bluestacks.com>
2020-07-24 18:43:49 +0530

    trying notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,14 +8,11 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    var flutter_native_splash = 1
     UIApplication.shared.isStatusBarHidden = false
-	FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
 	if #available(iOS 10.0, *) {
   // For iOS 10 display notification (sent via APNS)
 	UNUserNotificationCenter.current().delegate = self
-
 	let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
 	UNUserNotificationCenter.current().requestAuthorization(
     options: authOptions,
@@ -25,8 +22,8 @@ import Firebase
 	UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
 	application.registerUserNotificationSettings(settings)
 	}
-
-	
+    
+	application.registerForRemoteNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
@@ -34,3 +31,5 @@ import Firebase
       Messaging.messaging().apnsToken = deviceToken;
   }
 }
+
+
